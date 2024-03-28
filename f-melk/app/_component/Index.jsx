0b2ebb39b11e5415b18a14c7f08b7e3790558ab6
Saeed Jamali <mahdimem65@gmail.com>
2024-03-28 13:38:06 +0330

    0137
    0137

---
+++ f-melk/app/_component/Index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "../_component/Index.css"
+function Index() {
+  return (
+    <div className="Index_Content">
+     
+            <div className="Index_tt">
+                <button>
+                    اضافه کردن فایل جدید 
+                </button>
+            </div>
+            <div className="Index_tt">
+                <button>
+                    لیست فایلها 
+                </button>
+            </div>
+       
+    </div>
+  )
+}
+
+export default Index
---
+++ f-melk/app/page.js
@@ -0,0 +1,12 @@
+
+import Index from "./_component/Index";
+
+
+export default function Home() {
+  return (
+    
+    <div>
+    <Index/>
+    </div>
+  );
+}
